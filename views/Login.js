3f4ffa6619f519891d470d13742ca7b45f2b10d9
MiikaJok <miika.jokinen10@hotmail.com>
2023-09-26 14:28:54 +0300

    async kirjautuminen toimii

--- views/Login.js
+++ views/Login.js
@@ -1,15 +1,37 @@
 import {Button, Layout, Text, Divider} from '@ui-kitten/components';
 import PropTypes from 'prop-types';
-import { useContext, useState } from 'react';
-import { MainContext } from '../contexts/MainContext';
+import {useContext, useState, useEffect} from 'react';
+import {MainContext} from '../contexts/MainContext';
 import LoginForm from '../components/LoginForm';
 import RegisterForm from '../components/RegisterForm';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {useUser} from '../hooks/ApiHooks';
+import {KeyboardAvoidingView, TouchableOpacity, Keyboard} from 'react-native';
 
 const Login = ({navigation}) => {
-  const {setIsLoggedIn} = useContext(MainContext);
+  const {setIsLoggedIn, setUser} = useContext(MainContext);
+  const {getUserByToken} = useUser();
   const [isLoginFormVisible, setIsLoginFormVisible] = useState(false);
   const [isRegisterFormVisible, setIsRegisterFormVisible] = useState(false);
 
+  const checkToken = async () => {
+    try {
+      const token = await AsyncStorage.getItem('userToken');
+      const userData = await getUserByToken(token);
+      console.log('token', token);
+      console.log('userdata', userData);
+      if (userData) {
+        setIsLoggedIn(true);
+        setUser(userData);
+      }
+    } catch (error) {
+      console.log('checkToken', error);
+    }
+  };
+
+  useEffect(() => {
+    checkToken();
+  }, []);
 
   const showLoginForm = () => {
     setIsLoginFormVisible(true);
@@ -26,17 +48,13 @@ const Login = ({navigation}) => {
 
   return (
     <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Button
-      onPress={showLoginForm}
-      style={buttonStyle}
-      size='large'
-      >Login</Button>
-      <Divider style={{height: "20%"}} ></Divider>
-      <Button
-      onPress={showRegisterForm}
-      style={buttonStyle}
-      size='large'
-      >Or Register?</Button>
+      <Button onPress={showLoginForm} style={buttonStyle} size="large">
+        Login
+      </Button>
+      <Divider style={{height: '20%'}}></Divider>
+      <Button onPress={showRegisterForm} style={buttonStyle} size="large">
+        Or Register?
+      </Button>
       {/* Render the LoginForm as a modal */}
       {isLoginFormVisible && (
         <LoginForm
--- views/Profile.js
+++ views/Profile.js
@@ -1,19 +1,31 @@
 import {Button, Layout , Text} from '@ui-kitten/components';
 import { PropTypes } from 'prop-types';
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import { MainContext } from '../contexts/MainContext';
+import { ScrollView } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Profile = ({navigation}) => {
   const {setIsLoggedIn} = useContext(MainContext);
 
-  const logOut = () => {
-    setIsLoggedIn(false);
+  const logOut = async () => {
+    console.log('profile, logout');
+    try {
+      await AsyncStorage.clear();
+      setIsLoggedIn(false);
+    } catch (error) {
+      console.error(error);
+    }
   };
+  useEffect(() => {
+  }, []);
   return (
+    <ScrollView>
     <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
       <Text category="h1">Profile</Text>
       <Button onPress={logOut}>Logout</Button>
     </Layout>
+    </ScrollView>
   );
 };
 
