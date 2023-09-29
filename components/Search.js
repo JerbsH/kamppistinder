b9222ca2c64d9c0b13bcd76f5e714dfbb740ada7
ellasig <103430174+ellasig@users.noreply.github.com>
2023-09-29 12:31:32 +0300

    search on progress

--- components/Search.js
+++ components/Search.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { Layout, Input, FlatList, Text } from '@ui-kitten/components';
+import { Layout, Input, Text, Button, Card, Image } from '@ui-kitten/components';
+import { mediaUrl } from '../utils/app-config';
 
 export function Search({ mediaArray }) {
   const [searchQuery, setSearchQuery] = useState('');
@@ -14,8 +15,10 @@ export function Search({ mediaArray }) {
 
       const results = mediaArray.filter((media) =>
         media.title.toLowerCase().includes(searchQuery.toLowerCase()) ||
-        media.description.toLowerCase().includes(searchQuery.toLowerCase())
+        media.description.toLowerCase().includes(searchQuery.toLowerCase()) ||
+        (media.image && media.image.toLowerCase().includes(searchQuery.toLowerCase()))
       );
+
       setSearchResults(results);
     };
 
@@ -31,16 +34,13 @@ export function Search({ mediaArray }) {
         value={searchQuery}
       />
       {searchQuery !== '' ? (
-        <FlatList
-          data={searchResults}
-          keyExtractor={(item) => item.id.toString()}
-          renderItem={({ item }) => (
-            <Layout>
-              <Text>{item.title}</Text>
-              <Text>{item.description}</Text>
-            </Layout>
-          )}
-        />
+        searchResults.map((media) => (
+          <Card key={media.id}>
+            <Text>{media.title}</Text>
+            <Text>{media.description}</Text>
+            {media.image && <Image source={{ uri: mediaUrl }} style={{ width: 200, height: 200 }} />}
+          </Card>
+        ))
       ) : (
         <Text>No search results yet.</Text>
       )}
--- views/Home.js
+++ views/Home.js
@@ -1,17 +1,17 @@
-import React, {useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Layout, Text, Button, Card, Input } from '@ui-kitten/components';
 import PropTypes from 'prop-types';
 import Upload from './Upload';
 import List from '../components/List';
 import { useMedia } from '../hooks/ApiHooks';
-import { Search } from '../components/Search';
 
 const Home = ({ navigation }) => {
   const [uploadModalVisible, setUploadModalVisible] = useState(false);
   const [shouldCloseUpload, setShouldCloseUpload] = useState(false);
-  const [searchResults, setSearchResults] = useState([]); // Store search results
-  const [searchText, setSearchText] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
   const { mediaArray } = useMedia();
+  const [showList, setShowList] = useState(true); // Track when to show the list
 
   const toggleUploadModal = () => {
     setUploadModalVisible(!uploadModalVisible);
@@ -28,47 +28,33 @@ const Home = ({ navigation }) => {
     }
   }, [shouldCloseUpload]);
 
-  const handleSearchTextChange = (text) => {
-    setSearchText(text);
-  };
-
-  const performSearch = () => {
-    if (searchText === '') {
-      setSearchResults([]);
-      return;
+  useEffect(() => {
+    if (searchQuery !== '') {
+      const results = mediaArray.filter((media) =>
+        media.title.toLowerCase().includes(searchQuery.toLowerCase()) ||
+        media.description.toLowerCase().includes(searchQuery.toLowerCase())
+      );
+      setSearchResults(results);
+      setShowList(false); // Hide the list when search is active
+    } else {
+      setShowList(true); // Show the list when no search is performed
     }
-
-    const results = mediaArray.filter(
-      (media) =>
-        media.title.toLowerCase().includes(searchText.toLowerCase()) ||
-        media.description.toLowerCase().includes(searchText.toLowerCase())
-    );
-
-    setSearchResults(results);
-  };
+  }, [searchQuery, mediaArray]);
 
   return (
     <Layout style={{ flex: 1 }}>
-      <Input
-        placeholder="Search..."
-        value={searchText}
-        onChangeText={handleSearchTextChange}
-        onSubmitEditing={performSearch}
-        style={{ marginHorizontal: 16 }}
-      />
       <Layout
         style={{
           flexDirection: 'row',
           justifyContent: 'space-between',
           alignItems: 'center',
           paddingVertical: 10,
-          marginHorizontal: 16, // Add margin for spacing
+          marginHorizontal: 16,
         }}
       >
         <Button onPress={toggleUploadModal} style={{ flex: 1, marginHorizontal: 5 }}>
           Make a post
         </Button>
-
         <Button
           style={{ flex: 1, marginHorizontal: 5 }}
           onPress={() => {
@@ -86,13 +72,22 @@ const Home = ({ navigation }) => {
           onSuccess={uploadSuccessCallback}
         />
       )}
-      {searchResults.map((media) => (
-        <Card key={media.id}>
-          <Text>{media.title}</Text>
-          <Text>{media.description}</Text>
-        </Card>
-      ))}
-      <List/>
+      <Input
+        style={{ borderWidth: 1, padding: 8 }}
+        placeholder="Search..."
+        onChangeText={setSearchQuery}
+        value={searchQuery}
+      />
+      {showList ? (
+        <List />
+      ) : (
+        searchResults.map((media) => (
+          <Card key={media.id}>
+            <Text>{media.title}</Text>
+            <Text>{media.description}</Text>
+          </Card>
+        ))
+      )}
     </Layout>
   );
 };
