355233af3f3fca464912b22ed91d4f725fafc570
Jere <101744771+JerbsH@users.noreply.github.com>
2023-10-06 16:42:12 +0300

    splash screen

--- views/Comments.js
+++ views/Comments.js
@@ -1,32 +1,31 @@
-import React, { useState } from 'react';
-import { useComment, useUser } from '../hooks/ApiHooks';
-import { Divider } from 'react-native-elements';
+import React, {useState} from 'react';
+import {useComment, useUser} from '../hooks/ApiHooks';
+import {Divider} from 'react-native-elements';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { Button, Card, Input, Layout, Text } from '@ui-kitten/components';
-import { Image } from 'react-native';
-import { mediaUrl } from '../utils/app-config';
-import { useEffect } from 'react';
+import {Button, Card, Input, Layout, Text} from '@ui-kitten/components';
+import {Image} from 'react-native';
+import {mediaUrl} from '../utils/app-config';
+import {useEffect} from 'react';
 import {formatDate} from '../utils/functions';
 import PropTypes from 'prop-types';
 
-
-const Comments = ({ singleMedia, userId }) => {
-  const { postComment, getCommentsById, deleteComment } = useComment();
-  const { user } = useUser();
+const Comments = ({route}) => {
+  const {postComment, getCommentsById, deleteComment} = useComment();
+  const {user} = useUser();
   const [comments, setComments] = useState([]);
   const [userComments, setUserComments] = useState('');
 
+  const singleMedia = route.params;
 
+  useEffect(() => {
+    fetchComments();
+  }, [userComments]);
 
-useEffect(() => {
-  fetchComments();
-}, [userComments]);
-
-/* useEffect(() => {
+  /* useEffect(() => {
   fetchOwner();
 }, []); */
 
-/* // fetch owner info
+  /* // fetch owner info
 const fetchOwner = async () => {
   try {
     const token = await AsyncStorage.getItem('userToken');
@@ -64,7 +63,7 @@ const fetchOwner = async () => {
       if (response) {
         setUserComments(''); //Clear the comment input
         fetchComments();
-      };
+      }
     } catch (error) {
       console.error('Error posting comment:', error);
     }
@@ -80,18 +79,16 @@ const fetchOwner = async () => {
       console.error('Error deleting comment:', error);
     }
   };
-  console.log(singleMedia);
 
   return (
     <Card>
       <Divider />
       {comments.map((comment) => (
         <Layout key={comment.comment_id}>
-          <Layout style={{ flexDirection: 'row', alignItems: 'center' }}>
+          <Layout style={{flexDirection: 'row', alignItems: 'center'}}>
             <Text>{comment.comment}</Text>
             {user.user_id === comment.user_id && (
-              <Button
-                onPress={() => handleCommentDelete(comment.comment_id)}>
+              <Button onPress={() => handleCommentDelete(comment.comment_id)}>
                 Delete
               </Button>
             )}
@@ -102,9 +99,9 @@ const fetchOwner = async () => {
       {singleMedia && (
         <Layout>
           <Image
-            source={{ uri: mediaUrl + singleMedia.filename }}
+            source={{uri: mediaUrl + singleMedia.filename}}
             resizeMode="center"
-            style={{ height: 300 }}
+            style={{height: 300}}
           />
           <Text>Title: {singleMedia.title}</Text>
           <Text>Description: {singleMedia.description}</Text>
@@ -124,10 +121,7 @@ const fetchOwner = async () => {
 };
 
 Comments.propTypes = {
-  singleMedia: PropTypes.object,
-  userId: PropTypes.object,
+  route: PropTypes.object,
 };
 
-
 export default Comments;
-
