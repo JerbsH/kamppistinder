31f0fbaa66e98831884ccdf70a9d0aac86b2b034
Jere <101744771+JerbsH@users.noreply.github.com>
2023-10-11 10:58:39 +0300

    fixes

--- views/SwipeCards.js
+++ views/SwipeCards.js
@@ -26,7 +26,7 @@ const SwipeCards = () => {
   const {mediaArray, loadMedia} = useMedia();
   const {user} = useContext(MainContext);
 
-  // Reset notMyMedia when refreshing
+/*   // Reset notMyMedia when refreshing
   const resetNotMyMedia = () => {
     notMyMedia = mediaArray.filter((item) => {
       return (
@@ -35,6 +35,9 @@ const SwipeCards = () => {
       );
     });
   };
+  const [notMyMedia, setNotMyMedia] = useState([]); */
+
+  const [notMyMedia, setNotMyMedia] = useState([]); // Initialize notMyMedia as an empty array
 
   const handleRefresh = async () => {
     console.log('Refreshing...');
@@ -43,7 +46,8 @@ const SwipeCards = () => {
       setIndex(0);
       translateX.setValue(0);
       swipesRef.current = 0;
-      resetNotMyMedia();
+      //resetNotMyMedia();
+      fetchNotMyMedia();
     } catch (error) {
       console.error('Refresh failed', error);
     }
@@ -60,16 +64,17 @@ const SwipeCards = () => {
     }
   };
   useEffect(() => {
+    fetchNotMyMedia();
     fetchFavourites();
   }, []);
 
-  let notMyMedia = mediaArray.filter((item) => {
+  /* let notMyMedia = mediaArray.filter((item) => {
     // Check if the item is not in the favoriteMedia list
     return (
       !favouriteMedia.some((favorite) => favorite.file_id === item.file_id) &&
       item.user_id !== user.user_id
     );
-  });
+  }); */
 
   const [index, setIndex] = useState(0);
   const translateX = useMemo(() => new Animated.Value(0), []);
@@ -107,6 +112,12 @@ const SwipeCards = () => {
           console.error(error.message);
         }
 
+        // Remove the liked post from the notMyMedia array
+      const updatedNotMyMedia = [...notMyMedia];
+      updatedNotMyMedia.splice(index, 1);
+      setNotMyMedia(updatedNotMyMedia);
+
+
         Animated.timing(translateX, {
           toValue: width,
           duration: 400,
@@ -124,6 +135,21 @@ const SwipeCards = () => {
           topOffset: 10,
           visibilityTime: 1000,
         });
+
+        // Remove the disliked post from the notMyMedia array
+      const updatedNotMyMedia = [...notMyMedia];
+      updatedNotMyMedia.splice(index, 1);
+      setIndex((prevIndex) => (prevIndex + 1) % updatedNotMyMedia.length);
+
+      // Check if there are still cards to display
+      if (updatedNotMyMedia.length > 0) {
+        // Make sure the index does not exceed the array length
+        setIndex((prevIndex) => prevIndex % updatedNotMyMedia.length);
+      }
+
+      // Update the notMyMedia array
+      setNotMyMedia(updatedNotMyMedia);
+
         Animated.timing(translateX, {
           toValue: -width,
           duration: 400,
@@ -148,6 +174,17 @@ const SwipeCards = () => {
     () => notMyMedia[index] || {},
     [notMyMedia, index],
   );
+
+  const fetchNotMyMedia = () => {
+    const updatedNotMyMedia = mediaArray.filter((item) => {
+      return (
+        !favouriteMedia.some((favorite) => favorite.file_id === item.file_id) &&
+        item.user_id !== user.user_id
+      );
+    });
+    setNotMyMedia(updatedNotMyMedia);
+  };
+
   return (
     <GestureHandlerRootView style={styles.container}>
       <Toast />
